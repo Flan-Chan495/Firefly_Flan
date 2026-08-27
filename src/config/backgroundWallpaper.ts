37f99b451f419593ee66e584bf38e3fb065709ab
Flan-Chan495 <my1711825017@gmail.com>
2026-08-27 21:34:42 +0800

    content: update banner title and subtitle text

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -59,17 +59,12 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 			// 是否启用主页横幅文字
 			enable: true,
 			// 主页横幅主标题
-			title: "Lovely firefly!",
+			title: "Flan_Chan495 の Blog",
 			// 主页横幅主标题字体大小
 			titleSize: "4.5rem",
 			// 主页横幅副标题
 			subtitle: [
-				"In Reddened Chrysalis, I Once Rest",
-				"From Shattered Sky, I Free Fall",
-				"Amidst Silenced Stars, I Deep Sleep",
-				"Upon Lighted Fyrefly, I Soon Gaze",
-				"From Undreamt Night, I Thence Shine",
-				"In Finalized Morrow, I Full Bloom",
+				"ごきげんよう",
 			],
 			// 主页横幅副标题字体大小
 			subtitleSize: "1.5rem",
